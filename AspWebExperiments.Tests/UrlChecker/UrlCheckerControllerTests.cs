e21c9992627f9be8f5da8ccaeb36ffa4ff7537d7
Hehemon <hehemon@gmail.com>
2017-07-29 16:50:32 +0300

    AME-1 Included tests for controller/Statuses

--- AspWebExperiments.Tests/UrlChecker/UrlCheckerControllerTests.cs
+++ AspWebExperiments.Tests/UrlChecker/UrlCheckerControllerTests.cs
@@ -1,9 +1,11 @@
 ﻿using System.Web.Mvc;
 using System.Xml.XPath;
+using AspExperiments.Core.UrlChecker;
 using AspWebExperiments.Controllers;
 using AspWebExperiments.Models.UrlChecker;
 using AspWebExperiments.Tests.IoC;
 using AspWebExperiments.ViewModels.UrlChecker;
+using Moq;
 using StructureMap;
 using Xunit;
 
@@ -14,18 +16,24 @@ namespace AspWebExperiments.Tests.UrlChecker
         protected override Registry CreateContainerRegistry()
         {
             var registry =  base.CreateContainerRegistry();
+
+            var urlCheckerServiceMock = new Mock<IUrlCheckerService>();
+            urlCheckerServiceMock.Setup(m => m.IsUrlAvailable("ok", 1000)).Returns(true);
+            urlCheckerServiceMock.Setup(m => m.IsUrlAvailable("error", 1000)).Returns(false);
+            registry.For<IUrlCheckerService>().Use(context => urlCheckerServiceMock.Object);
+
             return registry;
         }
 
         [Theory]
-        [InlineData("https://www.google.by/images/branding/googlelogo/2x/googlelogo_color_120x44dp.png", true)]
-        [InlineData("https://www.google.by/images/branding/googlelogo/2x/googlelogo_color_120x44dp.png.Error", false)]
+        [InlineData("ok", true)]
+        [InlineData("error", false)]
         public void TestStatusView(string url, bool expectedResult)
         {
-            //var controller = new UrlCheckerController();
-            //var result = controller.Status(url) as ViewResult;
-            //Assert.NotNull(result);
-            //Assert.Equal(expectedResult, ((UrlCheckerStatusViewModel) result.Model).AvailableUrlStatuses[0].Status == SuccessStatus.Ok);
+            var controller = IoC.GetInstance<UrlCheckerController>();
+            var result = controller.Status(url) as ViewResult;
+            Assert.NotNull(result);
+            Assert.Equal(expectedResult, ((UrlCheckerStatusViewModel)result.Model).AvailableUrlStatuses[0].Status == SuccessStatus.Ok);
         }
     }
 }
